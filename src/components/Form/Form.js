2f2fdb14af051b8b15a95800999efe283f6d367a
lucascr <lcrodrigues1995@gmail.com>
2022-08-12 18:19:53 -0300

    Create Form.js

---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+const Form = () => {
+  <form>
+    <input type="text" placeholder="Book title..." />
+    <input type="text" placeholder="Author..." />
+    <button type="submit">Add Book</button>
+  </form>;
+};
+
+export default Form;
