d8934bd4de032590c9f79a0b372b9280be2cdc1a
lucascr <lcrodrigues1995@gmail.com>
2022-08-12 18:19:57 -0300

    Create Books.js

---
+++ src/Pages/Books.js
@@ -0,0 +1,11 @@
+import BookList from '../components/BookList/BookList';
+import Form from '../components/Form/Form';
+
+const Books = () => {
+  <div>
+    <BookList booksList={[]} />
+    <Form />
+  </div>;
+};
+
+export default Books;
