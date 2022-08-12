95e2ad52e521cdf074c93ae5b944e52845bd72d4
lucascr <lcrodrigues1995@gmail.com>
2022-08-12 18:19:49 -0300

    Create DefaultLayout.js

---
+++ src/components/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const DefaultLayout = ({ children }) => (
+  <section>
+    <header>
+      <nav>
+        <Link to="/">Books</Link>
+        <Link to="/categories">Categories</Link>
+      </nav>
+    </header>
+    <main>
+      {children}
+    </main>
+    <footer
+      style={{
+        backgroundColor: 'gray',
+        borderTop: '1px solid black',
+        height: '25px',
+      }}
+    />
+  </section>
+);
+
+DefaultLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DefaultLayout;
