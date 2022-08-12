590e15ebb79a9b3fdf67937cd7a3c2bf578f9e88
lucascr <lcrodrigues1995@gmail.com>
2022-08-12 19:52:09 -0300

    Add heading to the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,8 @@
 const Form = () => (
   <form>
+    <h3>
+      Add New Book
+    </h3>
     <input type="text" placeholder="Book title..." />
     <input type="text" placeholder="Author..." />
     <button type="submit">Add Book</button>
