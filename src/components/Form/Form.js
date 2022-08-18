e6e08de05467440bf34937d6192ee99b8de38ba4
lucascr <lcrodrigues1995@gmail.com>
2022-08-18 10:32:21 -0300

    add 'add' functionality

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,26 @@
-const Form = () => (
-  <form>
-    <h3>
-      Add New Book
-    </h3>
-    <input type="text" placeholder="Book title..." />
-    <input type="text" placeholder="Author..." />
-    <button type="submit">Add Book</button>
-  </form>
-);
+import { useDispatch } from 'react-redux/es/exports';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/books';
+
+const Form = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook(e.target.bTitle.value, e.target.bAuthor.value, uuidv4()));
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>
+        Add New Book
+      </h3>
+      <input type="text" name="bTitle" placeholder="Book title..." />
+      <input type="text" name="bAuthor" placeholder="Author..." />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
 
 export default Form;
