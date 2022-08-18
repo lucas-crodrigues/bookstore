ede148c2a6bac7098a2101fe5d069e66bc27b53f
lucascr <lcrodrigues1995@gmail.com>
2022-08-18 18:27:11 -0300

    getbooks from api

--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux/es/exports';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
 import BookList from '../components/BookList/BookList';
 import Form from '../components/Form/Form';
+import { getBooks } from '../redux/books/books';
 
 const Books = () => {
   const BooksList = useSelector((state) => state.books);
+
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getBooks());
+  }, []);
 
   return (
     <div>
