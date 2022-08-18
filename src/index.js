e60cdfd6c1757340ba79ab66dbee492471016f84
lucascr <lcrodrigues1995@gmail.com>
2022-08-18 10:32:35 -0300

    add provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import storeReducer from './redux/configureStore';
 import Bookstore from './Bookstore';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Bookstore />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={storeReducer}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Bookstore />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 );
