7d54e1748c11eb4e8e64a829f0d6d6369dd30fb9
lucascr <lcrodrigues1995@gmail.com>
2022-08-18 18:27:33 -0300

    update functionality to make api calls

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,42 +1,72 @@
 const ADD = 'BOOKSTORE/books/ADD';
 const REMOVE = 'BOOKSTORE/books/REMOVE';
+const FETCH = 'BOOKSTORE/books/FETCH';
 
-const booksList = [
-  {
-    title: 'The Alchemist',
-    author: 'Paulo Coelho',
-    id: '0',
-  },
-  {
-    title: 'The Lord of the Rings',
-    author: 'J.R.R. Tolkien',
-    id: '1',
-  },
-];
+const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QuMp8GsvtTpbGQVlti2B/books';
+
+const booksList = [];
 
 export default (state = booksList, action) => {
   switch (action.type) {
     case ADD:
-      return [...state, {
-        title: action.title,
-        author: action.author,
-        id: action.id,
-      },
-      ];
+      return [...state, action.book];
 
     case REMOVE:
-      return state.filter((book) => book.id !== action.id);
+      return state.filter((book) => book.item_id !== action.book.item_id);
+
+    case FETCH:
+      return action.payload;
 
     default:
       return state;
   }
 };
 
-export const addBook = (bookTitle, bookAuthor, bookId) => ({
-  type: ADD,
-  title: bookTitle,
-  author: bookAuthor,
-  id: bookId,
-});
+export const addBook = (book) => async (dispatch) => {
+  await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: book.item_id,
+      title: book.title,
+      author: book.author,
+      category: book.category,
+    }),
+  }).then(dispatch({
+    type: ADD,
+    book,
+  }));
+};
 
-export const removeBook = (bookId) => ({ type: REMOVE, id: bookId });
+export const removeBook = (book) => async (dispatch) => {
+  await fetch(`${BASE_URL}/${book.item_id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(dispatch({
+    type: REMOVE,
+    book,
+  }));
+};
+
+export const getBooks = () => async (dispatch) => {
+  const booksFetch = await fetch(BASE_URL, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  }).then((data) => data.json());
+
+  const books = [];
+
+  Object.keys(booksFetch).forEach((e) => {
+    books.push({ ...booksFetch[e][0], item_id: e });
+  });
+  dispatch({
+    type: FETCH,
+    payload: books,
+  });
+};
